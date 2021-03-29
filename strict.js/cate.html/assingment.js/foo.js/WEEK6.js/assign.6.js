a0d25ac6c3e94c364e2985915ff13842ce523560
gathonicatherine <catherrinewanjiku@gmail.com>
2021-03-29 09:07:06 +0300

    javascript_assignment6

---
+++ strict.js/cate.html/assingment.js/foo.js/WEEK6.js/assign.6.js
@@ -0,0 +1,18 @@
+var kiosk = {
+    set getFruits(fruit){
+        this.fruit= this.fruit+fruit;
+    },
+    fruits: ["Kiwi","Watermelon"]
+    
+}
+kiosk get.Fruits = "Orange"
+console.log(kiosk.fruit)
+    
+  
+
+
+
+
+
+ 
+
